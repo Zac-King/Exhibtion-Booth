092bdd6447b55badf121f6104c768f78f3250380
Zac King <ZacKingx@gmail.com>
2017-01-25 16:08:36 -0600

    pre-build

---
+++ Assets/ToggleVR.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleVR : MonoBehaviour
+{
+    GvrViewer m_gvr;
+
+    void Start()
+    {
+        m_gvr = GetComponent<GvrViewer>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+            m_gvr.VRModeEnabled = !m_gvr.VRModeEnabled;
+    }
+
+}
